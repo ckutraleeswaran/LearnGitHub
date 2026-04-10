1decf3de72a65b8e631fd0cc8497124445aa427b
ckutraleeswaran <kutraleeswaranc.gmail.com>
2026-04-10 13:28:28 +0530

    maths.java -- by yeamini

--- src/learnGitHub/org/Maths.java
+++ src/learnGitHub/org/Maths.java
@@ -10,10 +10,28 @@ public class Maths {
 	//3. multi
 	//4. division
 	
-	
-	
+	public void addition() {	//addition completed
+		
+		int a = 10;
+		int b = 22;
+		int c = a + b;
+			System.out.println("The 'c' value is  : " + c) ;
+	}
+	public void sub() {			//substraction completed
+		
+		int a = 22;
+		int b = 10;
+		int c = a - b;
+			System.out.println("The 'c' value is  : " + c) ;
+	}
 	
 	public static void main(String[] args) {	//This is main method
+		
+		Maths obj = new Maths();
+		
+		obj.addition();
+		obj.sub();
+		
 		
 	}
 	

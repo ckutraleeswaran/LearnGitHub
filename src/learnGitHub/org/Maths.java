190bf26a347580ea714548f60944fab8f5f8ac4d
ckutraleeswaran <kutraleeswaranc.gmail.com>
2026-04-10 12:29:28 +0530

    maths project- by manager

---
+++ src/learnGitHub/org/Maths.java
@@ -0,0 +1,21 @@
+package learnGitHub.org;
+
+public class Maths {
+	
+	
+	
+	///This project is created by Kutraleeswaran
+	//1. addition
+	//2. sub
+	//3. multi
+	//4. division
+	
+	
+	
+	
+	public static void main(String[] args) {	//This is main method
+		
+	}
+	
+
+}

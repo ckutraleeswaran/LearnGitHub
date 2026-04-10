1376e415b4432e9ff2b2e7821723ca33995ef4ee
ckutraleeswaran <kutraleeswaranc.gmail.com>
2026-04-10 13:50:30 +0530

    this task completed by saranya

--- src/learnGitHub/org/Maths.java
+++ src/learnGitHub/org/Maths.java
@@ -9,12 +9,29 @@ public class Maths {
 	//2. sub
 	//3. multi
 	//4. division
-	
+	public void multi() {	//multi completed
+		
+		int a = 10;
+		int b = 22;
+		int c = a * b;
+			System.out.println("The 'c' value is  : " + c) ;
+	}
+	public void division() {			//division completed
+		
+		int a = 20;
+		int b = 10;
+		int c = a / b;
+			System.out.println("The 'c' value is  : " + c) ;
+			
+	}
 	
 	
 	
 	public static void main(String[] args) {	//This is main method
 		
+	
+		obj.multi();
+		obj.division();
 	}
 	
 
